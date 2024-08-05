0dc88b5da45bc94db40a8054606808268c4a868a
AakifKhan7 <baloochaakifkhan@gamil.com>
2024-08-05 23:43:44 +0530

    completed

--- DAY 18/colorgram.py
+++
@@ -1,4 +0,0 @@
-import colorgram
-
-colors = colorgram.extract('hello.avif', 6)
-print(colors)
--- DAY 19/turtle_racing.py
+++ DAY 19/turtle_racing.py
@@ -1,25 +1,36 @@
 from turtle import Turtle, Screen
 import random
 
-screen = Screen() 
-random_walk = random.randint(5, 10)
-red = Turtle()
-red.color("red")
-red.speed("slow")
-red.goto(-150, 0)
-blue = Turtle()
-blue.color("blue")
-blue.speed("slow")
-for i in range(500):
-    red.forward(random_walk)
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="make your bet" , prompt="which turtle will won the race ? Enter a color.")
+color = ["red", "orange", "yellow","green", "blue", "purple"] 
+y_position = [-70, -40, -10, 20, 50, 80]
+all_turtle = []
 
-# blue = Turtle()
-# blue.color("blue")
-
-# def random_Walk():
-#     global red
-#     r_walk = red.forward(random.randint(5, 10))
-#     return r_walk
-
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(color[turtle_index])
+    tim.penup()
+    tim.goto(-230, y_position[turtle_index])
+    all_turtle.append(tim)
+    
+if user_bet:
+    is_race_on = True
+    
+while is_race_on:
+    
+    for turtle in all_turtle:
+        if turtle.xcor()> 220:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {turtle} turtle is winner.")
+            else:
+                print(f"You've lost! The {turtle} turtle is winner.")
+        turtle.pendown()
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+        
 screen.exitonclick()
-# random_Walk()
